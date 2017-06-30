e2c11ba608ea551b4b54efad2eb13ddd87b904b1
Zirak Mondegari <codewizduo@gmail.com>
2017-06-30 23:28:23 +0530

    Create 530.cpp

---
+++ Practice-1/530.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    void solve(TreeNode *root, int &min_diff, int &val) {
+        if (root -> left != NULL) {
+            solve(root -> left, min_diff, val);
+        }
+        if (val >= 0) {
+            min_diff = min(min_diff, (root -> val) - val);
+        }
+        val = root -> val;
+        if (root -> right != NULL) {
+            solve(root -> right, min_diff, val);
+        }
+    }
+    
+    int getMinimumDifference(TreeNode *root) {
+		int min_diff = INT_MAX;
+        int val = -1;
+        solve(root, min_diff, val);
+        return min_diff;
+    }
+};
