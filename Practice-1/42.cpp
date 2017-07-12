ee9327d4c11928f0886f378ebf1c6d5dc5ced398
Zirak Mondegari <codewizduo@gmail.com>
2017-07-13 00:02:11 +0530

    Create 42.cpp

---
+++ Practice-1/42.cpp
@@ -0,0 +1,29 @@
+//Trapping Rain Water
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int left = 0, right = n - 1;
+        int max_left = 0, max_right = 0;
+        int ans = 0;
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= max_left) {
+                    max_left = height[left];
+                } else {
+                    ans += (max_left - height[left]);    
+                }
+                left++;
+            } else {
+                if (height[right] >= max_right) {
+                    max_right = height[right];
+                } else {
+                    ans += (max_right - height[right]);   
+                }  
+                right--;
+            }
+        }
+        return ans;
+    }
+};
