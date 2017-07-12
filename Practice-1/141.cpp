d5477591ff34b243bd8fd8bb43fd4887d1da479e
Zirak Mondegari <codewizduo@gmail.com>
2017-07-13 00:37:08 +0530

    Create 141.cpp

---
+++ Practice-1/141.cpp
@@ -0,0 +1,31 @@
+//Linked List Cycle
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == NULL || head -> next == NULL) {
+            return false;
+        }
+        ListNode *slow = head -> next;
+        ListNode *fast = head -> next -> next;
+        while (slow != NULL && fast != NULL && slow != fast) {
+            slow = slow -> next;
+            if (fast -> next == NULL) {
+                return false;
+            }
+            fast = fast -> next -> next;
+        }
+        if (slow != NULL && slow == fast) {
+            return true;
+        }
+        return false;
+    }
+};
