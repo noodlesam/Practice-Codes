4dd7f5fc83dcd8515ae6388fd7f24052b2eb296a
Zirak Mondegari <codewizduo@gmail.com>
2017-07-13 23:35:26 +0530

    Create 186.cpp

---
+++ Practice-1/186.cpp
@@ -0,0 +1,16 @@
+//Reverse Words in a String II
+
+class Solution {
+public:
+    void reverseWords(string &s) {
+        reverse(s.begin(), s.end());
+        int start = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == ' ') {
+                reverse(s.begin() + start, s.begin() + i);
+                start = i + 1;
+            }
+        }
+        reverse(s.begin() + start, s.end());
+    }
+};
